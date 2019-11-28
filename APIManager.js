d47d4438390502b50226d9fe9c663ea0bbf2c211
Betz <silverbez@gmail.com>
2019-11-28 15:59:09 +0200

    added fetch users

--- APIManager.js
+++ APIManager.js
@@ -1,12 +1,26 @@
 //This is the class that will manage all your APIs
 
 class APIManager {
-    constructor() {
-        this.data = {}
-    }
+  constructor() {
+    this.data = {};
+  }
 
-    loadData(){
-        //you should make all your API requests here
-        //each request should update the `data` object accordingly
-    }
+  loadData() {
+    //you should make all your API requests here
+    //each request should update the `data` object accordingly
+
+    this.fetchUsers(7);
+  }
+
+
+  fetchUsers(numUsers) {
+    $.ajax({
+      context: this, //opens our access to - this
+      url: `https://randomuser.me/api/?results=${numUsers}`,
+      success: function(usersResult) {
+        this.data.users = usersResult;
+        console.log(this.data);
+      }
+    });
+  }
 }
--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 // Create instances of your classes
 // Create the loadData and renderData functions - these should use the relevant instance
+
+const apiManager = new APIManager()
+const renderer = new Renderer()
+
+apiManager.loadData()
