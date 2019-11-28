b6cf0582f5eb52d759b2ff94410261ded2efa706
Betz <silverbez@gmail.com>
2019-11-28 19:27:46 +0200

    added all API requestes

--- APIManager.js
+++ APIManager.js
@@ -3,13 +3,17 @@
 class APIManager {
   constructor() {
     this.data = {};
+    this.totalPokemons = 806
   }
 
   loadData() {
     //you should make all your API requests here
     //each request should update the `data` object accordingly
 
-    this.fetchUsers(7);
+    this.fetchUsers(7)
+    this.fetchRandomKanyeQuote()
+    this.fetchRandomPokemonData(this.totalPokemons)
+    this.fetchRandomText()
   }
 
 
@@ -17,10 +21,49 @@ class APIManager {
     $.ajax({
       context: this, //opens our access to - this
       url: `https://randomuser.me/api/?results=${numUsers}`,
-      success: function(usersResult) {
-        this.data.users = usersResult;
-        console.log(this.data);
+      success: function(result) {
+        this.data.users = result
+        console.log(this.data.users)
       }
     });
   }
+
+  fetchRandomKanyeQuote() {
+    $.ajax({
+      context: this, //opens our access to - this
+      url: `https://api.kanye.rest/`,
+      success: function(result) {
+        this.data.quote = result
+        console.log(this.data.quote)
+      }
+    });
+  }
+
+  fetchRandomPokemonData(totalPokes) {
+    let id = Math.floor(Math.random() * totalPokes) + 1  
+    $.ajax({
+      context: this, //opens our access to - this
+      url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
+      success: function(result) {
+        // this.data.pokemonData = result.map(res => res.species.name);
+        let pokemon = {name: result.species.name, image: result.sprites.front_default} 
+        this.data.pokemonData = pokemon
+        console.log(this.data.pokemonData)
+      }
+    });
+  }
+
+  fetchRandomText() {
+    $.ajax({
+      context: this, //opens our access to - this
+      url: `https://baconipsum.com/api/?type=all-meat`,
+      success: function(result) {
+        // this.data.pokemonData = result.map(res => res.species.name);
+        this.data.meatText = result
+        console.log(this.data.meatText)
+      }
+    });
+  }
+  
+  
 }
